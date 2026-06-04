b5b55cb2d71ed421f71c612c2cc62bdb547e7f13
Sebastian <darkopx56@gmail.com>
2026-06-04 17:14:43 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: [
-      'pasapalabra-gvkv.onrender.com',
+      'https://pasapalabra-gvkv.onrender.com/',
       'localhost'
     ]
   }
