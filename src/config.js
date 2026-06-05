88c6b0312b6fe652937a99a459dd2b8f5abdbe56
Sebastian <166878493+Darkops56@users.noreply.github.com>
2026-06-05 16:03:49 -0300

    update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,4 @@
 // Configuración de las URLs para la API backend y el servidor de WebSockets (Socket.IO).
 // Si cambias el puerto en el servidor (server/index.js), edita este archivo.
-
-const BACKEND_PORT = '10000';
-const HOST = 'https://pasapalabra-1.onrender.com';
-
-export const API_BASE_URL = `http://${HOST}:${BACKEND_PORT}`;
-export const SOCKET_URL = `http://${HOST}:${BACKEND_PORT}`;
+export const API_BASE_URL = 'https://pasapalabra-1.onrender.com';
+export const SOCKET_URL = 'https://pasapalabra-1.onrender.com';
