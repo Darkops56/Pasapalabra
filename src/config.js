708281fd6ee7303f89888b6f412a236348f76b2a
Sebastian <166878493+Darkops56@users.noreply.github.com>
2026-06-05 15:55:42 -0300

    configuracion del puerto

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Configuración de las URLs para la API backend y el servidor de WebSockets (Socket.IO).
 // Si cambias el puerto en el servidor (server/index.js), edita este archivo.
 
-const BACKEND_PORT = '3001';
+const BACKEND_PORT = '10000';
 const HOST = 'https://pasapalabra-1.onrender.com';
 
 export const API_BASE_URL = `http://${HOST}:${BACKEND_PORT}`;
